ade4e6fcab4c3be077b29016ceab8c50b95c0dac
joegasewicz <joegoasewicz@gmail.com>
2022-11-09 15:33:41 +0000

    Should handle all methods #4

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -1,32 +1,64 @@
 from bobtail.wsgi import BobTail
+from bobtail.request import Request
 from .mock_environ import mock_environ
+
 
 def test_bobtail():
     class Images:
 
-        def get(self):
-            return f"It Works!"
+        def get(self, req: Request):
+            return {
+                       "users": [{"id: 1"}, {"id: 2"}]
+                   }, 200
 
-        def post(self):
-            pass
+        def post(self, req: Request):
+            return None, 202
 
-        def delete(self):
-            pass
+        def delete(self, req: Request):
+            return None, 201
 
-        def put(self):
-            pass
+        def put(self, req: Request):
+            return {
+                       "id": 1,
+                   }, 202
 
+        def patch(self, req: Request):
+            return {
+                       "id": 1,
+                   }, 202
 
     routes = [
-        (Images, "/images")
+        (Images(), "/images")
     ]
 
     app = BobTail(routes=routes)
 
-    def start_response(status, response_headers):
-        pass
+    # GET
+    environ = {"PATH_INFO": "/images", "REQUEST_METHOD": "GET"}
+    data = app(environ, lambda s, r: None)
+    assert data == [b"{'users': [{'id: 1'}, {'id: 2'}]}"]
+    assert app.response.status == 200
 
-    res = app(mock_environ, start_response)
-    assert res == [b"Hello World!\n"]
+    # POST
+    environ = {"PATH_INFO": "/images", "REQUEST_METHOD": "POST"}
+    data = app(environ, lambda s, r: None)
+    assert data is None
+    assert app.response.status == 202
 
+    # DELETE
+    environ = {"PATH_INFO": "/images", "REQUEST_METHOD": "DELETE"}
+    data = app(environ, lambda s, r: None)
+    assert data is None
+    assert app.response.status == 201
 
+    # PUT
+    environ = {"PATH_INFO": "/images", "REQUEST_METHOD": "PUT"}
+    data = app(environ, lambda s, r: None)
+    assert data == [b"{'id': 1}"]
+    assert app.response.status == 202
+
+    # PATCH
+    environ = {"PATH_INFO": "/images", "REQUEST_METHOD": "PATCH"}
+    data = app(environ, lambda s, r: None)
+    assert data == [b"{'id': 1}"]
+    assert app.response.status == 202
