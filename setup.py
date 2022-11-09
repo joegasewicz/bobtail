14d3186b0e5b7fd560be44d4569807ec1734f149
joegasewicz <joegoasewicz@gmail.com>
2022-11-09 13:18:04 +0000

    0.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     install_requires=[
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
