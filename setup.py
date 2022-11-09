56cae63ebc44bafb43c8ee0210d60d669787b132
joegasewicz <joegoasewicz@gmail.com>
2022-11-09 13:07:14 +0000

    v0.0.1

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="bobtail",
+    version="0.0.1",
+    description="A little Python http framework",
+    packages=["bobtail"],
+    py_modules=["bobtail"],
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joegasewicz/bobtail",
+    author="Joe Gasewicz",
+    author_email="joegasewicz@gmail.com",
+)
