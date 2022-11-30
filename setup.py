365bb1a7fea919a1d72002792b14c36767e9d032
joegasewicz <joegasewicz@gmail.com>
2022-11-30 11:25:12 +0000

    Release 0.0.9 - updates setup.py to require birman

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bobtail",
-    version="0.0.8",
+    version="0.0.9",
     description="A little Python http framework",
     packages=["bobtail"],
     py_modules=["bobtail"],
-    install_requires=[
+    requires=[
         "birman"
     ],
     classifiers=[
