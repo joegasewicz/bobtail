6bb5b08dc9a1ee1b138a704c877ab42094639a90
joegasewicz <joegasewicz@gmail.com>
2022-12-02 13:15:53 +0000

    Files #37 Release 0.0.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bobtail",
-    version="0.0.9",
+    version="0.0.10",
     description="A little Python http framework",
     packages=["bobtail"],
     py_modules=["bobtail"],
