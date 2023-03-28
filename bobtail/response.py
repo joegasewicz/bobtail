6aa995d2aa806eb033f243ed9a0abefb7cd3449d
joegasewicz <joegasewicz@gmail.com>
2023-03-28 20:27:35 +0100

    static files + config #63

--- bobtail/response.py
+++ bobtail/response.py
@@ -3,6 +3,9 @@ import json
 
 
 class Response:
+    """
+    The :class:`Response` object is available from within a Route method.
+    """
     status: int = 200
 
     body: Dict = None
@@ -21,27 +24,19 @@ class Response:
         """Process final response"""
         if self.body is not None:
             resp_data = bytes(json.dumps(self.body, indent=2), "utf-8")
-            self.set_content_len(resp_data)
+            self._set_content_len(resp_data)
             return [resp_data]
         if self.html is not None:
             resp_data = bytes(self.html, "utf-8")
-            self.set_content_len(resp_data)
+            self._set_content_len(resp_data)
             return [resp_data]
         if self.static is not None:
-            self.set_content_len(self.static)
+            self._set_content_len(self.static)
             return [self.static]
-        self.set_content_len(None)
+        self._set_content_len(None)
         return []
 
-    def set_content_len(self, data: Optional[bytes]) -> None:
-        """
-        Warning: calling this method directly inside a route handler will have no effect
-        as the content length is set dynamically after the execution of the route handler
-        :param data:
-        :type data:
-        :return:
-        :rtype:
-        """
+    def _set_content_len(self, data: Optional[bytes]) -> None:
         if self._content_length is None:
             con_len = 0 if data is None else len(data)
             self.headers = {
@@ -51,10 +46,17 @@ class Response:
 
     def set_headers(self, value: Dict) -> None:
         """
+        Headers can be set from within a route method. The `Content-Type` header get
+        set dynamically from :class:`Response` methods. By default, `Content-Type` is
+        set to `application/json`.
+        For Example::
+
+            def get(self, req: Request, res: Response):
+                res.set_headers({
+                    "Content-Type": "application/json",
+                })
+
         :param value: Excepts a dict
-        :type value:
-        :return:
-        :rtype:
         """
         self.headers = {
             **self.headers,
@@ -63,36 +65,56 @@ class Response:
 
     def set_status(self, value: int) -> None:
         """
+        Set the response status.
+        You can set the status with the Response object's set_status method.
+        The default status is always set to 200 if there are no errors.
+        For Example::
+
+            def get(self, req: Request, res: Response):
+                res.set_headers({"Content-type": "text/plain"})
+
         :param value: Sets the response status
-        :type value:
-        :return:
-        :rtype:
         """
         self.status = value
 
     def set_body(self, value: Dict) -> None:
         """
-        Set the body of the request
+        Set the body of the request.
+        For example::
+
+            def get(self, req: Request, res: Response):
+                res.set_body({id: 1})
+
         :param value:
-        :type value:
-        :return:
-        :rtype:
         """
         self.body = value
 
-    def set_html(self, template_str) -> None:
+    def set_html(self, template_str: str) -> None:
         """
-        :param value:
-        :param template:
-        :return:
+        Renders an HTML string.
+        For example::
+
+            template_str = "<h1>Hello!</h1>"
+
+            def get(self, req: Request, res: Response):
+                res.set_html(template_str)
+
+        :param template_str:
         """
         self.set_headers({"Content-Type": "text/html"})
         self.html = template_str
 
     def set_static(self, path: str):
         """
+        Calling `set_status` from within a route method will render a static
+        file such as a .css, .js or a media type file. The :class:`~BaseOptions`
+        class sets the `STATIC_DIR` directory.
+        For example::
+
+                def get(self, req: Request, res: Response) -> None:
+                    res.set_static("cat1.jpg")
+
         :param path:
-        :return:
         """
         self.set_headers({"Content-Type": "image/jpeg"})
         file = open(path, "rb")
