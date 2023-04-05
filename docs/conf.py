f663583e5f042ddce96cd3093f78e939d2eae24b
joegasewicz <joegasewicz@gmail.com>
2023-04-05 20:31:07 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,12 +27,12 @@ extensions = [
 autodoc_mock_imports = ["birman"]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
