c5338e414d2ff3a9be5dfde56f1f9219efc47f5f
joegasewicz <joegasewicz@gmail.com>
2023-04-05 01:36:48 +0100

    static files + config #63

--- README.md
+++ README.md
@@ -209,6 +209,60 @@ except MultipartFormDataError:
     pass # handle no multipart form value
 ```
 
+### Static Files
+To declare a static route postfix a `*` to the route's path::
+```python
+from bobtail import BobTail AbstractRoute, BaseOptions
+from bobtail_jinja2 import BobtailJinja2
+
+routes = [
+    (Static(), "/static/*"),
+]
+
+class Options(BaseOptions):
+    STATIC_DIR = "app/static"
+    TEMPLATE_DIR = "app/templates"
+
+blog = BobTail(routes=routes, options=Options())
+blog.use(BobtailJinja2(template_dir="app/templates"))
+```
+Calling `set_static` from within a route method will render a static
+file such as a .css, .js or a media type file. The :class:`~BaseOptions`
+class sets the `STATIC_DIR` directory.
+```python
+
+    class Static(AbstractRoute):
+        def get(self, req: Request, res: Response) -> None:
+            res.set_static("/static/imgs/cat1.jpg")
+```
+You can set the static file path using the :class:`~BaseOptions`.
+```python
+
+class Options(BaseOptions):
+    STATIC_DIR = "/static"
+
+# Now in a route handler we can access static directory the via options
+class Static(AbstractRoute):
+    def get(self, req: Request, res: Response) -> None:
+        res.set_static(f"{res.options.STATIC_DIR}/imgs/cat1.jpg")
+```
+By default, `STATIC_DIR` is set to `/static`, if your static file is nested
+within a Python package, for example `app/static` the set as `STATIC_DIR = "app/static"`
+
+To render an image from within a Jinja2 template include the full path including the
+static directory name or path. For example::
+```html
+<!-- if STATIC_DIR = "/static" -->
+<body>
+    <img src="/static/imgs/cat1.jpg" />
+</body>
+```
+OR without the first forward slash::
+```html
+<body>
+    <img src="static/imgs/cat1.jpg" />
+</body>
+```
 ### OOP Approach
 If you prefer to organise your routes in a more OOP approach, you can implement the
 `AbstractRoute` abstract class. It's especially useful when using an IDE like Pycharm
--- bobtail/response.py
+++ bobtail/response.py
@@ -114,13 +114,58 @@ class Response:
 
     def set_static(self, path: str):
         """
-        Calling `set_status` from within a route method will render a static
+        To declare a static route postfix a `*` to the route's path::
+
+            from bobtail import BobTail AbstractRoute, BaseOptions
+            from bobtail_jinja2 import BobtailJinja2
+
+            routes = [
+                (Static(), "/static/*"),
+            ]
+
+            class Options(BaseOptions):
+                STATIC_DIR = "app/static"
+                TEMPLATE_DIR = "app/templates"
+
+            blog = BobTail(routes=routes, options=Options())
+            blog.use(BobtailJinja2(template_dir="app/templates"))
+
+        Calling `set_static` from within a route method will render a static
         file such as a .css, .js or a media type file. The :class:`~BaseOptions`
         class sets the `STATIC_DIR` directory.
         For example::
 
-                def get(self, req: Request, res: Response) -> None:
-                    res.set_static("cat1.jpg")
+                class Static(AbstractRoute):
+                    def get(self, req: Request, res: Response) -> None:
+                        res.set_static("/static/imgs/cat1.jpg")
+
+        You can set the static file path using the :class:`~BaseOptions`.
+        For example::
+
+                class Options(BaseOptions):
+                    STATIC_DIR = "/static"
+
+                # Now in a route handler we can access static directory the via options
+                class Static(AbstractRoute):
+                    def get(self, req: Request, res: Response) -> None:
+                        res.set_static(f"{res.options.STATIC_DIR}/imgs/cat1.jpg")
+
+        By default, `STATIC_DIR` is set to `/static`, if your static file is nested
+        within a Python package, for example `app/static` the set as `STATIC_DIR = "app/static"`
+
+        To render an image from within a Jinja2 template include the full path including the
+        static directory name or path. For example::
+
+            <!-- if STATIC_DIR = "/static" -->
+            <body>
+                <img src="/static/imgs/cat1.jpg" />
+            </body>
+
+        OR without the first forward slash::
+
+            <body>
+                <img src="static/imgs/cat1.jpg" />
+            </body>
 
         :param path:
         """
