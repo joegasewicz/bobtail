23ef99bcbd7b2f2d43b5b14315daea68fa0ff9db
joegasewicz <joegasewicz@gmail.com>
2024-03-07 00:20:22 +0000

    #86 v0.0.24

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bobtail",
-    version="0.0.23",
+    version="0.0.24",
     description="A little Python http framework",
     packages=["bobtail"],
     py_modules=["bobtail"],
