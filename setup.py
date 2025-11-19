3f86cef44d625d6ace263242acedab24ec969b95
joegasewicz <joegasewicz@gmail.com>
2025-11-19 11:32:07 +0000

    v0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bobtail",
-    version="0.2.1",
+    version="0.2.2",
     description="A little Python http framework",
     packages=["bobtail"],
     py_modules=["bobtail"],
